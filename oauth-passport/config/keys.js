91c0ed62ff8ff69b45e4aa24d381e83facd550bf
NihalMaddela127 <33574689+NihalMaddela127@users.noreply.github.com>
2018-01-05 07:54:58 +0530

    passport auth - google+
    passport service - google+ auth, cookie duration, new and old user management in database - (mlab mongodb online service) for sample project.

---
+++ oauth-passport/config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: '297706472479-53u89ekmruf3fil3g1nmius95mckjvgc.apps.googleusercontent.com',
+        clientSecret: 'z5dsHdrbSVo4bj02cfPWbVg8'
+    },
+    mongodb:{
+        dbURI:'mongodb://nihalmaddela:test@ds239217.mlab.com:39217/oauth-project-db'
+    },
+    session:{
+        cookieKey:'nihalfanofmessi'
+    }
+};
